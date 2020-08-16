a6e76f00627524531e25da47749731971b6c57af
unknown <phungquocviet07@gmail.com>
2020-08-16 13:21:20 +0700

    create all enum

---
+++ Sources/LibraryManagement/Model/BookType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    enum BookType
+    {
+        Textbook,
+        Autobiography,
+        Thriller,
+        Dictionary,
+        Comic,
+        Poem,
+        Novel,
+        Science,
+        Psychology,
+        ExerciseBook,
+        Horror,
+        ReferenceBook,
+        Encyclopedia,
+        Magazine,
+        Programming,
+        Mathematic
+    }
+}
