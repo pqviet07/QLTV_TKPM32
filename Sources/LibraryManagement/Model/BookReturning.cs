b05b343f79bf55aff62b474173f0b89ceb113fb8
unknown <hoangvinh175@gmail.com>
2020-08-16 15:13:49 +0700

    them class Book, BookItem, Rack, booklending, bookreturning,bookReturningDetail

---
+++ Sources/LibraryManagement/Model/BookReturning.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Model
+{
+    class BookReturning
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Barcode { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public int? Duration { get; set; }
+        public decimal? Fine { get; set; }
+        [Required]
+        public ReturningStatus Status { get; set; }
+        public string Note { get; set; }
+    }
+}
